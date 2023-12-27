06466e73b245a25a781be9e4f25d17bf005209ac
MaksGorobets <gorobets542@gmail.com>
2023-12-27 18:37:07 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Maks Winters on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
