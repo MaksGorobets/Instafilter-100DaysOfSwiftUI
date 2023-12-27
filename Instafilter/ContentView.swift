e6856dca10002c64f58dc6a590c90c6b5c9e28f1
MaksGorobets <gorobets542@gmail.com>
2023-12-27 19:45:09 +0200

    Day 63

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,18 +5,78 @@
 //  Created by Maks Winters on 27.12.2023.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentFilter = CIFilter.pixellate()
+    
+    @State private var amount = 1.0
+    
+    @State private var image: Image?
+    @State private var uiImage: UIImage?
+    @State private var isPicking = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if image != nil {
+                Spacer()
+                image?
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+            } else {
+                ContentUnavailableView("Pick an image", systemImage: "photo.badge.plus")
+            }
+            Slider(value: $amount, in: 0.1...1)
+                .onChange(of: amount) { oldValue, newValue in
+                    loadImage()
+                }
+                .padding()
+            Button("Pick an image") {
+                isPicking.toggle()
+            }
+            .sheet(isPresented: $isPicking, content: {
+                ImagePicker(uiImage: $uiImage, isPresenting: $isPicking)
+                    .onDisappear(perform: {
+                        loadImage()
+                    })
+            })
         }
-        .padding()
     }
+    
+    func loadImage() {
+        guard let inputImage = uiImage else { return }
+        let ciImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        
+        currentFilter.inputImage = ciImage
+        currentFilter.scale = Float(amount * 100)
+        //        let amount = 1.0
+        //        let inputKeys = currentFilter.inputKeys
+        
+        //        if inputKeys.contains(kCIInputIntensityKey) {
+        //            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        //        }
+        //
+        //        if inputKeys.contains(kCIInputRadiusKey) {
+        //            currentFilter.setValue(amount * 360, forKey: kCIInputRadiusKey)
+        //        }
+        //
+        //        if inputKeys.contains(kCIInputScaleKey) {
+        //            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        //        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
+    }
+    
 }
 
 #Preview {
---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,83 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Maks Winters on 27.12.2023.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+
+  @Binding var uiImage: UIImage?
+  @Binding var isPresenting: Bool
+   
+
+  func makeUIViewController(context: Context) -> UIImagePickerController {
+
+    let imagePicker = UIImagePickerController()
+
+    imagePicker.sourceType = .photoLibrary
+
+    imagePicker.delegate = context.coordinator
+
+    return imagePicker
+
+  }
+
+
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+  }
+
+ 
+  typealias UIViewControllerType = UIImagePickerController
+  
+
+  func makeCoordinator() -> Coordinator {
+
+    Coordinator(self)
+
+  }
+   
+
+  class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+     
+
+    let parent: ImagePicker
+
+         
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+      parent.uiImage = info[.originalImage] as? UIImage
+
+      parent.isPresenting = false
+
+    }
+
+     
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+
+      parent.isPresenting = false
+
+    }
+
+     
+
+    init(_ imagePicker: ImagePicker) {
+
+      self.parent = imagePicker
+
+    }
+
+     
+
+  }
+
+
+}
